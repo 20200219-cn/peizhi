7e5a01e383449a448142bd4c6fb13f0600b45b33
20200219-cn <527417756@qq.com>
2025-10-28 14:34:50 +0800

    update something

--- script.js
+++ script.js
@@ -13,7 +13,3 @@
       }
     });
   }
-
-
-// Optional JS to dynamically change banner content or adjust speed
-document.querySelector('.banner-content').textContent = '欢迎来到我的个人主页!';
--- tests/script.js
+++
@@ -1,2 +0,0 @@
-// Optional JS to dynamically change banner content or adjust speed
-document.querySelector('.banner-content').textContent = 'Attention is all you need!';
