e1e8a153c6d90d00cd4566acaa22b8dd4b9a6e0d
20200219-cn <527417756@qq.com>
2025-10-28 13:00:26 +0800

    update for BP

--- script.js
+++ script.js
@@ -13,3 +13,7 @@
       }
     });
   }
+
+
+// Optional JS to dynamically change banner content or adjust speed
+document.querySelector('.banner-content').textContent = '欢迎MINISO团队，详情点击右上方“工作流”按钮查看!';
---
+++ tests/script.js
@@ -0,0 +1,2 @@
+// Optional JS to dynamically change banner content or adjust speed
+document.querySelector('.banner-content').textContent = 'Attention is all you need!';
